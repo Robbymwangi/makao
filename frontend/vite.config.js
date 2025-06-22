ac2010b055ddb2d5777b7546159cd9409247b744
Robbymwangi <138704364+Robbymwangi@users.noreply.github.com>
2025-06-22 13:29:27 +0300

    Fix WebSocket HMR connection error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,4 +10,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    host: true,
+    hmr: {
+      clientPort: 443,
+      protocol: 'wss'
+    }
+  }
 })
